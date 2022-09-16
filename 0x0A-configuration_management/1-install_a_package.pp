853139845c318bb841014f64b6a17cd713c67bbd
kwamboka3 <pkwamboka@flag42.com>
2022-09-16 15:21:21 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  name     => 'puppet-lint',
-  provider => 'gem'
+# installs the package flask
+package { 'pip3':
+  ensure   => 'installed'
+}
+
+exec { 'pip install flask':
+  command => '/usr/bin/pip3 install flask',
+}
+
+package { 'flask':
+  ensure => '2.1.0'
 }
