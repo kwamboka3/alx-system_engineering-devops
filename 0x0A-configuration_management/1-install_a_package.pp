cfd9c29249ff95cb5c4a1fbe0910fef60c833553
kwamboka3 <pkwamboka@flag42.com>
2022-09-21 17:16:27 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# installs the package flask
-package { 'pip3':
-  ensure   => 'installed'
-}
-
-exec { 'pip install flask':
-  command => '/usr/bin/pip3 install flask',
-}
-
-package { 'flask':
-  ensure => '2.1.0'
+# install flask
+package{'flask':
+ensure   => '2.1.0',
+provider => 'pip3'
 }
