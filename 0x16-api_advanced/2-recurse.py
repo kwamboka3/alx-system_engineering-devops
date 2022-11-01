8c3d20a650131003aac8993e31c4c9c70bcb261b
kwamboka3 <pkwamboka@flag42.com>
2022-11-01 16:46:04 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts on a given Reddit subreddit."""
-
+""" Queries the Reddit API """
 import requests
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+
+def recurse(subreddit, hot_list=[]):
+    """ Prints the titles of an amount of hot posts
+        listed for a given subreddit.
+        solve with recursion, by using the 'after' request.
+    """
+    user = {"User-Agent": "Scoot"}
+    if len(hot_list) is 0:
+        req = requests.get("https://www.reddit.com/r/{}/hot.json"
+                           .format(subreddit), headers=user)
+    else:
+        req = requests.get("https://www.reddit.com/r/{}/hot.json?after={}_{}"
+                           .format((subreddit),
+                                   hot_list[-1].get('kind'),
+                                   hot_list[-1].get('data').get('id')),
+                           headers=user)
+    if req.status_code != requests.codes.OK:
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    if len(req.json().get('data').get('children')) < 1:
+        return hot_list
+    hot_list.extend(req.json().get('data').get('children'))
+    return recurse(subreddit, hot_list)
