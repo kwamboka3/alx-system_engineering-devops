044cc53fe47ad315c3cbda278a63f42f183aee2c
kwamboka3 <pkwamboka@flag42.com>
2023-02-28 16:39:17 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-""" Queries the Reddit API """
+"""
+    Queries Reddit's API to find the number of subscribers for a given
+    subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Returns the number of subscribers for a given subreddit.
-        If an invalid subreddit is given, return 0.
+    """ Makes a query to Reddit's API for a given subreddit and returns the
+        number of subscribers to that subreddit.
+    Args:
+        subreddit (str): The name of the subreddit that will be used to query
+        the API.
+    Returns:
+        Returns the number of subscribers to the subreddit.
+        Returns 0 if the subreddit does not exist.
     """
-      url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+    (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+    headers = {'User-Agent': user_agent}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
-        return 0
+    resp = requests.get('https://www.reddit.com/r/{}/new/.json?limit=1'
+                        .format(subreddit),
+                        headers=headers,
+                        allow_redirects=False)
+    subscribers = 0
+    if resp.status_code == 200:
+        a_dict = resp.json()
+        a_dict = a_dict.get('data', {})
+        a_list = a_dict.get('children', [])
+        if len(a_list) != 0:
+            a_dict = a_list[0]
+            a_dict = a_dict.get('data', {})
+            subscribers = a_dict.get('subreddit_subscribers', 0)
+    return subscribers
